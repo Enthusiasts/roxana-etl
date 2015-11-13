282efc84bda47d2ac002ebf7dc4b032830c72e0b
Dmitry Volkov <mrdaemonsmile@gmail.com>
2015-11-14 00:37:11 +0300

    extract added

--- boot.py
+++ boot.py
@@ -10,10 +10,10 @@ __author__ = 'debalid'
 
 logging.basicConfig(filename="roxana-etl" + ".log", level=logging.DEBUG, format='%(asctime)s %(message)s')
 
-(json_data_mos_ru, json_instagram) = extract(data_mos_ru=os.path.join(os.getcwd(), "raw/data_mos_ru"),
+(json_data_mos_ru, json_instagram, json_polygons) = extract(data_mos_ru=os.path.join(os.getcwd(), "raw/data_mos_ru"),
                                              instagram=os.path.join(os.getcwd(), "raw/instagram"))
 
-(entertainments, checkins, times, clients, zones) = transform(json_data_mos_ru, json_instagram)
+(entertainments, checkins, times, clients, zones) = transform(json_data_mos_ru, json_instagram, json_polygons)
 
 load(entertainments, checkins, times, clients, zones)
 
